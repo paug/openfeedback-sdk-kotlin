d2fedcf913e9a03c01dcdc40e155a75a06feee87
Martin Bonnin <martin@mbonnin.net>
2020-03-31 02:42:04 +0200

    version is now 0.0.5-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ buildscript {
     }
 }
 
-version = "0.0.4"
+version = "0.0.5-SNAPSHOT"
 
 subprojects {
     repositories {
@@ -98,7 +98,7 @@ fun Project.configurePublishing() {
                 pom {
                     groupId = "io.openfeedback"
                     artifactId = values.artifactName
-                    version = "0.0.4"
+                    version = "0.0.5-SNAPSHOT"
 
                     name.set(artifactId)
                     packaging = "aar"
