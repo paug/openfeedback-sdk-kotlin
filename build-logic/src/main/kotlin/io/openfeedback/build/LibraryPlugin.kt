8a8603dfb0d2baf377ee24520e233c1c685f62ba
Martin Bonnin <martin@mbonnin.net>
2022-04-15 16:52:58 +0200

    fix Github url

--- build-logic/src/main/kotlin/io/openfeedback/build/LibraryPlugin.kt
+++ build-logic/src/main/kotlin/io/openfeedback/build/LibraryPlugin.kt
@@ -112,18 +112,18 @@ private fun Project.configurePublishingInternal(artifactName: String) {
                             name.set(artifactId)
                             packaging = "aar"
                             description.set(artifactId)
-                            url.set("https://github.com/martinbonnin/openfeedback-android-sdk")
+                            url.set("https://github.com/paug/openfeedback-android-sdk")
 
                             scm { scm ->
-                                scm.url.set("https://github.com/martinbonnin/openfeedback-android-sdk")
-                                scm.connection.set("https://github.com/martinbonnin/openfeedback-android-sdk")
-                                scm.developerConnection.set("https://github.com/martinbonnin/openfeedback-android-sdk")
+                                scm.url.set("https://github.com/paug/openfeedback-android-sdk")
+                                scm.connection.set("https://github.com/paug/openfeedback-android-sdk")
+                                scm.developerConnection.set("https://github.com/paug/openfeedback-android-sdk")
                             }
 
                             licenses { licenseSpec ->
                                 licenseSpec.license { license ->
                                     license.name.set("MIT License")
-                                    license.url.set("https://github.com/martinbonnin/openfeedback-android-sdk/blob/master/LICENSE")
+                                    license.url.set("https://github.com/paug/openfeedback-android-sdk/blob/master/LICENSE")
                                 }
                             }
 
