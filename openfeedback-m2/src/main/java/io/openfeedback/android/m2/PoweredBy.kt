deb2d45b8c6cd5d6837cc827ecf6e51775b07671
Gerard Paligot <gerard.paligot@gmail.com>
2022-12-16 23:44:56 +0100

    feat: Localize Powered by string.

--- openfeedback-m2/src/main/java/io/openfeedback/android/m2/PoweredBy.kt
+++ openfeedback-m2/src/main/java/io/openfeedback/android/m2/PoweredBy.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.TextStyle
@@ -28,14 +29,15 @@ fun PoweredBy(
     val logo =
         if (MaterialTheme.colors.isLight) R.drawable.openfeedback_light
         else R.drawable.openfeedback_dark
+    val poweredBy = stringResource(id = R.string.powered_by)
     Row(
         modifier = modifier.semantics(mergeDescendants = true) {
-            contentDescription = "Powered by Openfeedback"
+            contentDescription = "$poweredBy Openfeedback"
         },
         horizontalArrangement = Arrangement.spacedBy(4.dp),
         verticalAlignment = Alignment.Top
     ) {
-        Text(text = "Powered by", style = style, color = color)
+        Text(text = poweredBy, style = style, color = color)
         Image(
             painter = painterResource(id = logo),
             contentDescription = null,
