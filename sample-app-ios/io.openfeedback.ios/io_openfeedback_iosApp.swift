8a08d272e55d378787984ad9457c4db649af8c06
Martin Bonnin <martin@mbonnin.net>
2024-03-24 22:21:03 +0100

    Add iOS app

---
+++ sample-app-ios/io.openfeedback.ios/io_openfeedback_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  io_openfeedback_iosApp.swift
+//  io.openfeedback.ios
+//
+//  Created by Martin on 24/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct io_openfeedback_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
